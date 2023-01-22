ca77670aa1423ad3ed99811ef6281ae88f3c8b2e
AjoyRoyy <ajoy.roy.ar.ar@gmail.com>
2023-01-22 16:01:07 +0600

    api updated

--- src/Pages/Dashboard/ManageServices/ManageServices.js
+++ src/Pages/Dashboard/ManageServices/ManageServices.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const ManageServices = () => {
     const [services, setServices] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:5000/services')
+        fetch('https://car-hab.onrender.com/services')
             .then(res => res.json())
             .then(data => setServices(data))
     }, []);
     const handleDelete = id => {
-        const url = `http://localhost:5000/services/${id}`;
+        const url = `https://car-hab.onrender.com/services/${id}`;
         fetch(url, {
             method: 'DELETE'
         })
